fe9af18825687f0b388aa15d054620bbfab6bb82
doffylamingo <102771222+doffylamingo@users.noreply.github.com>
2025-02-26 14:26:56 +0800

    feat(backend): set up pgx library for PostgreSQL connection

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"log"
+
 	"github.com/PersonalComputerOne/Phish-On/internal/routes"
+	"github.com/PersonalComputerOne/Phish-On/pkg/db"
 )
 
 func main() {
+	pool, err := db.Init()
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	defer pool.Close()
+
 	r := routes.SetupRouter()
 
 	r.Run(":8080")
