2c421054664e22211098e7479fc0b6f07f42d861
doffylamingo <102771222+doffylamingo@users.noreply.github.com>
2025-02-28 21:33:41 +0800

    feat(extension): initialize chrome extension with build setup

---
+++ extension/src/landing/components/footer.tsx
@@ -0,0 +1,9 @@
+import "../styles/header-footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Phish On! All rights reserved.</p>
+    </footer>
+  );
+}
