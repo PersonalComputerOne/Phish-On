4e0e71fadb67408a746d0c8cf51a937241e3a8d0
doffylamingo <102771222+doffylamingo@users.noreply.github.com>
2025-03-30 19:50:15 +0800

    fish+rod (#14)
    Co-authored-by: personalcomputerone <johnmarvinramos18@gmail.com>

--- extension/js/hello.js
+++ extension/js/hello.js
@@ -1,35 +1,42 @@
 const greetings = [
-    "👋 Hello", "👋 Hola", "👋 Bonjour", "👋 Hallo", "👋 Ciao", 
-    "👋 こんにちは", "👋 안녕하세요", "👋 你好", "👋 Привет", "👋 مرحبا"
+  "👋 Hello",
+  "👋 Hola",
+  "👋 Bonjour",
+  "👋 Hallo",
+  "👋 Ciao",
+  "👋 こんにちは",
+  "👋 안녕하세요",
+  "👋 你好",
+  "👋 Привет",
+  "👋 مرحبا",
 ];
 
 let index = 0;
 const helloText = document.getElementById("hello-text");
 
 setInterval(() => {
+  helloText.style.opacity = 1;
+  setTimeout(() => {
+    index = (index + 1) % greetings.length;
+    helloText.textContent = greetings[index];
     helloText.style.opacity = 1;
-    setTimeout(() => {
-        index = (index + 1) % greetings.length;
-        helloText.textContent = greetings[index];
-        helloText.style.opacity = 1;
-    }, 100);
+  }, 100);
 }, 100);
 
-window.onload = function() {
-    const loadingScreen = document.getElementById("loading-screen");
-    const content = document.getElementById("content");
+window.onload = function () {
+  const loadingScreen = document.getElementById("loading-screen");
+  const content = document.getElementById("content");
 
-    // Fade out loading screen
-    loadingScreen.style.opacity = "1";
+  // Fade out loading screen
+  loadingScreen.style.opacity = "1";
+  setTimeout(() => {
+    loadingScreen.style.display = "none";
+    content.style.display = "block";
     setTimeout(() => {
-        loadingScreen.style.display = "none";
-        content.style.display = "block";
-        setTimeout(() => {
-            content.style.opacity = "1";
-            content.style.transform = "translateY(0)";
-        }, 300);
-    }, 1500); 
-    
+      content.style.opacity = "1";
+      content.style.transform = "translateY(0)";
+    }, 300);
+  }, 1500);
 };
 
 const images = document.querySelectorAll(".carousel-image"); // Select carousel images
@@ -55,3 +62,24 @@ images.forEach((image) => {
 
 // Add event listener to the whole document to enable scrolling when clicking anywhere
 document.addEventListener("click", enableScroll);
+
+setTimeout(() => {
+  const cursorDot = document.querySelector(".dot"); // Ensure it's properly selected
+  const Line = document.querySelector(".dot-img"); // Ensure it's properly selected
+
+  window.addEventListener("mousemove", function (e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    Line.animate(
+      {
+        left: `${posX}px`,
+        top: `${posY}px`,
+      },
+      { duration: 500, fill: "forwards" }
+    );
+  });
+}, 3000); // Delay by 3 seconds
